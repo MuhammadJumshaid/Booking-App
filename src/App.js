e89a1b8ec31094aab09216b4d8fd020e9ca76d7b
unknown <jumshaid.khan.1222@gmail.com>
2024-08-16 11:24:11 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import Home from "./Pages/Home/Home";
-import Topbar from "./Components/Topbar/Topbar";
-import Single from "./Pages/Home/Single/Single";
-import Write from "./Pages/write/Write";
-import Settings from "./Pages/settings/Settings";
-import Login from "./Pages/login/Login";
-import Register from "./Pages/register/Register";
+
 import React from "react";
-
-
+import Home from "./Pages/home/Home";
+import List from "./Pages/list/List";
+import Hotel from "./Pages/hotel/Hotel";
 
 import {
 
@@ -21,45 +16,17 @@ import {
 
 
 
+
 function App() {
-  // const user = true;
-  const user = false;
+ 
   return (
-  <>
-      
-
-    <BrowserRouter>
-    <Topbar />
-      <Routes>
-        <Route exact path="/" element = {<Home/>}/>
-        <Route path="/register" element = {  user ? <Home /> : <Register/>}/>
-        <Route path="/login" element = {user ? <Home /> :<Login/>}/>
-        <Route path="/write" element = { user ? <Write /> : <Register/>}/>
-        <Route path="/settings" element = { user ? <Settings /> : <Register/>}/>Z
-        <Route path="/post/:postId"element = {<Single  />}/>
-      </Routes>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element = {<Home/>}></Route> 
+    <Route path="/list" element = {<List/>}></Route> 
+    <Route path="/hotel/:id" element = {<Hotel/>}></Route> 
+  </Routes>  
     </BrowserRouter>
-
-
-
-    {/* <BrowserRouter >
-    <Topbar />
-      <Routes>
-      
-        <Route exact path="/"> < Home/> </Route>
-        
-        <Route path="/register" >
-        {user ? <Home /> : <Register/>}
-          </Route>
-        <Route path="/login" > < Login/></Route>
-        <Route path="/write" > < Write/> </Route>
-        <Route path="/settings" > < Settings/> </Route>
-        <Route path="/post/:postId" > <Single/> </Route>
-        
-      </Routes>
-      </BrowserRouter> */}
-  
-    </>
     
   
   );
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,18 +1,29 @@
-import Header from '../../Components/Header/Header'
-import Posts from '../../Components/Posts/Posts'
-import Sidebar from '../../Components/Sidbar/Sidebar'
-import './Home.css'
+import React from 'react'
+import './home.css'
+import Navbar from '../../Components/navbar/Navbar'
+import Header from '../../Components/header/Header'
+import Featured from '../../Components/featured/Featured'
+import PropertyList from '../../Components/propertyList/PropertyList'
+import FeaturedProperty from '../../Components/featuredProperty/FeaturedProperty'
+import MailList from '../../Components/mailList/MailList'
+import Footer from '../../Components/footer/Footer'
 
 export default function Home() {
   return (
-    <>
-    <Header />
-    <div className='home'>
-    <Posts />
-    <Sidebar />
-    
-      
+    <div>
+      <Navbar />
+      <Header />
+      <div className='homeContainer'>
+        <Featured />
+        <h1 className='homeTitle'>Browse my property type</h1>
+        <PropertyList />
+        <h1 className='homeTitle'>Home guests love</h1>
+        <FeaturedProperty/>
+        <MailList />
+        <Footer/>
+        
+      </div>
+     
     </div>
-    </>
   )
 }
